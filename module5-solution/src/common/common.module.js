1bf64de6dfb8fe081bfb180f3e21f7c94cf0b038
Richi999 <navickasrich@gmail.com>
2020-11-26 11:03:04 +0200

    commiting amendments

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://guarded-island-75510.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
